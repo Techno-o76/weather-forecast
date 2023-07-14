90a94fba425f8f8632c7f444c3d64ee94769374a
Thanmai <thanmaisunku@gmail.com>
2023-07-14 07:26:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import FetchData from './components/FetchData';
+import FetchData2 from './components/FetchData2';
+import Hourly from './components/Hourly'
+import './App.css';
+// import Main from './components/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='App'>
+      
+        <Routes>
+          <Route path="/" element={<FetchData2 />} />
+          <Route path="/about" element={<FetchData />} />
+          <Route path="/hourly" element={<Hourly/>}/>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
